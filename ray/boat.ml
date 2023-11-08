937a8645b572b1cc980f07d223b4798d4907f92c
Parker Rho <pkr47@cornell.edu>
2023-11-08 00:51:18 -0500

    Add files from "Re-introduce modularity" commit
     - Re-commit with the new added files because they didn't push last commit

---
+++ ray/boat.ml
@@ -0,0 +1,33 @@
+open Raylib
+
+(** The signature of the boat character. *)
+module type BoatSig = sig
+  val boat_pos : Vector2.t ref
+  val boat_h : Vector2.t
+  val boat_v : Vector2.t
+  val boat_face : Vector2.t ref
+  val move : Vector2.t -> unit
+  val draw : unit -> unit
+end
+
+(** The module used for controlling the boat. *)
+module Boat : BoatSig = struct
+  let boat_pos = ref (Vector2.create 400. 225.)
+  let boat_h = Vector2.create 15. 10.
+  let boat_v = Vector2.create 10. 15.
+  let boat_face = ref boat_h
+
+  let move (v : Vector2.t) : unit =
+    if Vector2.x v = 0. then boat_face := boat_v else boat_face := boat_h;
+    boat_pos := Vector2.add !boat_pos v
+
+  let draw (() : unit) : unit =
+    (* The x and y coordinates of the center of the boat for easy reference. *)
+    let boatx = int_of_float (Vector2.x !boat_pos) in
+    let boaty = int_of_float (Vector2.y !boat_pos) in
+    (* The horiztonal and vertical radii respectively of the ellipse that
+       represents the boat. *)
+    let radh = Vector2.x !boat_face in
+    let radv = Vector2.y !boat_face in
+    draw_ellipse boatx boaty radh radv Color.brown
+end
---
+++ ray/boat.mli
@@ -0,0 +1,31 @@
+open Raylib
+
+(** The signature of the boat character. *)
+module type BoatSig = sig
+    val boat_pos : Vector2.t ref
+    (** The vector value for the current position of the center of the boat in the
+        window. *)
+  
+    val boat_h : Vector2.t
+    (** Represents the orientation of the boat with respect to an ellpise. boat_h
+        specifically represents the boat's orientation when it is moving left and
+        right. The x value corresponds with the horizontal radius and the y value
+        corresponds with the vertical radius. *)
+  
+    val boat_v : Vector2.t
+    (** Using the same format at boat_h, boat_v represents the boat's orientation
+        when it moves up and down.*)
+  
+    val boat_face : Vector2.t ref
+    (** The reference to the boat's current orientation. Set to boat_h by default. *)
+
+    val move : Vector2.t -> unit
+    (** Given a movement vector and a bool which is true when the boat is facing 
+        horizontally and false when facing vertically, move the boat accordingly. *)
+
+    val draw : unit -> unit
+    (** Draws the boat at its current position and orientation and colors it brown. *)
+  end
+
+(** The module used for controlling the boat. *)
+module Boat : BoatSig
