6bfceb749123fdbc0b484b2ddc98a11391b17fb8
Sennet Senadheera <sas639@cornell.edu>
2023-11-17 04:42:53 -0500

    Add starting background map
    - Create new folder for sprite collection
    - Create new 2D texture using Raylib for starting texture

--- ray/window.ml
+++ ray/window.ml
@@ -8,14 +8,15 @@ open Fish
 let current_fish = ref (Fish.spawn ())
 
 let setup () =
-  Raylib.init_window 800 450 "raylib [core] example - basic window";
+  Raylib.init_window 512 512 "raylib [core] example - basic window";
   Raylib.set_target_fps 60
 
 let rec loop () =
   if Raylib.window_should_close () then Raylib.close_window ()
   else
-    let open Raylib in
     (* Responding to key presses. *)
+    let texture_background = load_texture "data/sprites/bkg1.png" in 
+    draw_texture texture_background 0 0 Color.raywhite;
     if is_key_down Key.A then Boat.move (Vector2.create ~-.2. 0.) else ();
     if is_key_down Key.D then Boat.move (Vector2.create 2. 0.) else ();
     if is_key_down Key.W then Boat.move (Vector2.create 0. ~-.2.) else ();
