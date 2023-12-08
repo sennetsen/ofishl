42d4e7739ad5652623c8171e35a1b120cf7549ca
Sennet Senadheera <sas639@cornell.edu>
2023-12-08 16:01:54 -0500

    Start adding basic test cases
    - Add two test cases for a terminalentry.ml function

--- test/main.ml
+++ test/main.ml
@@ -1,2 +1,19 @@
 open OUnit2
 open Game
+open Terminalentry
+
+
+let terminal_tests =
+  [
+    ( "make_fish_string" >:: fun _ ->
+      let expected_fish = "🐟🐟🐟" in
+      let fish = make_fish_string "🐟" 3 "" in
+      assert_equal expected_fish fish );
+    ( "make_fish_string_zero_count" >:: fun _ ->
+      let expected_fish = "" in
+      let fish = make_fish_string "🐟" 0 "" in
+      assert_equal expected_fish fish );
+  ]
+
+let suite = "Final project test suite" >::: List.flatten [ terminal_tests ]
+let () = run_test_tt_main suite
